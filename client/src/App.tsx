3e89032c224b65ef91793f72eb3da5ac6448fb0e
Noothan am <100135382+Noothan-am@users.noreply.github.com>
2023-12-03 17:59:41 +0530

    adds nameset 2 page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -4,13 +4,12 @@ import Login from "./pages/Login";
 import "./index.css";
 import Home from "./pages/Home";
 import Forms from "./pages/Forms";
-import AuditPage from "./pages/AuditPage";
-import NamingSet from "./pages/NamingSet";
+import NamingSet2 from "./pages/NamingSet2";
 
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<NamingSet />} />
+      <Route path="/" element={<NamingSet2 />} />
       <Route path="/login" element={<Login />} />
       <Route path="/home" element={<Home />} />
       <Route path="/form" element={<Forms />} />

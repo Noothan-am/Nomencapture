879032ed205e6f17403b4558cbe3d38711f9b6d7
noothan-am <noothanam13@gmail.com>
2024-04-17 23:49:37 +0530

    fixes routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,8 +23,7 @@ import "./index.css";
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<SecondRoundGreetings />} />
-      {/* <Route path="/" element={<Login />} /> */}
+      <Route path="/" element={<Login />} />
       <Route path="/login" element={<Login />} />
       <Route path="/admin" element={<AdminDashboard />} />
       <Route element={<AdminProtectedRoutes />}>
